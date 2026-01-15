ce010c56ef08f2fe1053a5f7f723801ea7126b1f
Kristian Kristiansen <kriztiank@gmail.com>
2026-01-15 09:15:54 +0000

    Grid Layout & Responsive Design

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,27 @@ import "./style.css";
 
 export function App() {
   return (
-    <div className="flex items-center justify-between bg-slate-500 p-4">
-      <div className="font-bold">Logo</div>
-      <div className="flex gap-2">
-        <span>Home</span>
-        <span>About</span>
-        <span>Contact</span>
+    <div>
+      {/* Nav (flex) */}
+      <div className="flex items-center justify-between bg-slate-500 p-4">
+        <div className="font-bold">Logo</div>
+        <div className="flex gap-2">
+          <span>Home</span>
+          <span>About</span>
+          <span>Contact</span>
+        </div>
+      </div>
+      {/* Content (grid) */}
+      {/* Responsive Design */}
+      {/* boxes sm=1 md=2 lg=3 */}
+      {/* text sm=2xl anything bigger=text-sm */}
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 p-6 gap-6 text-center font-semibold text-2xl sm:text-sm">
+        <div className="bg-slate-400 p-4 rounded">Feature</div>
+        <div className="bg-slate-400 p-4 rounded">Feature</div>
+        <div className="bg-slate-400 p-4 rounded">Feature</div>
+        <div className="bg-slate-400 p-4 rounded">Feature</div>
+        <div className="bg-slate-400 p-4 rounded">Feature</div>
+        <div className="bg-slate-400 p-4 rounded">Feature</div>
       </div>
     </div>
   );
