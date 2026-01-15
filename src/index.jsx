6ec83c39a5421971c71204870651c98a97e37814
Kristian Kristiansen <kriztiank@gmail.com>
2026-01-15 07:49:49 +0000

    clean up

--- src/index.jsx
+++ src/index.jsx
@@ -1,25 +1,12 @@
 import { render } from "preact";
-import { LocationProvider, Router, Route } from "preact-iso";
-
-import { Header } from "./components/Header.jsx";
-import { Home } from "./pages/Home/index.jsx";
-import { NotFound } from "./pages/_404.jsx";
 import "./style.css";
 
 export function App() {
   return (
-    <div>
-      <p>Hello Worldaaa</p>
+    <div className="bg-blue-200">
+      Hello
     </div>
-    // <LocationProvider>
-    // 	<Header />
-    // 	<main>
-    // 		<Router>
-    // 			<Route path="/" component={Home} />
-    // 			<Route default component={NotFound} />
-    // 		</Router>
-    // 	</main>
-    // </LocationProvider>
+
   );
 }
 
