62fa7b63a6bf4a171073377eb3d398f1a3754ece
Kristian Kristiansen <kriztiank@gmail.com>
2026-01-15 08:23:59 +0000

    Nav (flex)

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,14 @@ import "./style.css";
 
 export function App() {
   return (
-    <div className="bg-blue-200">
-      Hello
+    <div className="flex items-center justify-between bg-slate-500 p-4">
+      <div className="font-bold">Logo</div>
+      <div className="flex gap-2">
+        <span>Home</span>
+        <span>About</span>
+        <span>Contact</span>
+      </div>
     </div>
-
   );
 }
 
